630a5d3e15ca41c02221a6cea58e22e67f6e2a04
Lasith Dissanayake <86358728+DissanayakeLYB@users.noreply.github.com>
2023-10-14 00:26:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ lambda_range = upper_range - lower_range
 #limit of the y
 y_limit = input("Threshold peak intensity : ")
 
+#x,y coordinates
+x_values = (input("x values (with space in between) : ")).split(" ")
+y_values = (input("y values (with space in between) : ")).split(" ")
+
+#program
 for i in range(lambda_range):
 
     if i > y_limit:
